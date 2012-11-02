582961e37e0282ee764eeb6e037be6487d1796f3
Fernando Guillen <fguillen.mail@gmail.com>
2012-11-02 18:24:32 +0100

    New OtaEnroll gem version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 OTAEnrollApp::Application.routes.draw do
   root :to => "profiles#new"
-
   resource :profile, :only => [:new, :show]
+
+  mount OtaEnroll::Engine => "/ota_enroll", :as => "ota_enroll_engine"
 end
